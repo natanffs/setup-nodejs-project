778e97e9820a01490ad76d63eeb079e95825e33b
natanffs <67486614+natanffs@users.noreply.github.com>
2020-07-17 13:39:21 -0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,9 @@
 import indexRoute from './index.routes'
 import authRoute from './auth.routes'
 import userRoute from './user.routes'
-import lessonRoute from './lesson.routes'
 
 export {
     indexRoute as index,
     authRoute as auth,
-    userRoute as user,
-    lessonRoute as lesson,
+    userRoute as user
 }
